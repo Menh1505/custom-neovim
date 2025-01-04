a07fcfc158c34e234867b002ba61d35ea47fe823
menh <dinhthienmenh1505@gmail.com>
2025-01-04 21:47:40 +0700

    simple lsp

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require "plugins" -- Load plugins from plugins/
 require "colorscheme" -- Autoload theme
 require "options"
 require "keymaps"
+require("lspconfig.health").check()
+require("nvim-lsp-installer.health").check()
