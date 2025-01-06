a584a481bb6e049df94c77c27d3570f7e0914e40
menh <dinhthienmenh1505@gmail.com>
2025-01-06 22:13:03 +0700

    config lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+local nowaitOpts = { noremap = true, silent = true, nowait = true }
+local keymap = vim.keymap.set
+
+-- Copy - Past - Select all
+keymap("n", "<C-a>", "ggVG$", nowaitOpts)
+keymap("i", "<C-a>", "<ESC>ggVG$i", nowaitOpts)
+keymap("v", "<C-c>", "y", { noremap = true, nowait = true })
+keymap("n", "<C-v>", "p", nowaitOpts)
+keymap("i", "<C-v>", "<ESC>pi", nowaitOpts)
+
+-- Press `` fast to exit insert mode
+keymap("i", "``", "<ESC>", opts)
+keymap("v", "``", "<ESC>", opts)
+keymap("n", "<C-q>", ":q<CR>", nowaitOpts)
+keymap("t", "<C-q>", [[<C-\><C-n>]], opts) -- Exit insert mode interminal
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,63 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt -- setting
+local g = vim.g --setting global
+
+vim.g.lazyvim_picker = "telescope"
+
+-- Padding
+if vim.g.neovide then
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  g.neovide_transparency = 0.95 -- Transparent
+  g.neovide_window_blurred = true
+
+  g.neovide_floating_blur_amount_x = 2.0
+  g.neovide_floating_blur_amount_y = 2.0
+
+  g.neovide_scroll_animation_length = 0.5
+  g.neovide_scroll_animation_far_lines = 1
+
+  g.neovide_cursor_animation_length = 0.3
+  g.neovide_cursor_trail_size = 0.5
+  g.neovide_cursor_vfx_mode = "railgun"
+  g.neovide_cursor_vfx_opacity = 500.0
+  g.neovide_cursor_vfx_particle_lifetime = 5.0
+  g.neovide_cursor_vfx_particle_phase = 1.5
+end
+
+opt.guifont = "0xProto Nerd Font" -- Font
+
+opt.backup = false -- creates a backup file
+opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+
+scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.title = true
+opt.autoindent = true
+opt.smartindent = true
+opt.hlsearch = true
+opt.showcmd = true
+opt.cmdheight = 0
+opt.laststatus = 0
+opt.expandtab = true
+opt.scrolloff = 10
+opt.inccommand = "split"
+opt.ignorecase = true
+opt.smarttab = true
+opt.breakindent = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.wrap = false
+opt.backspace = { "start", "eol", "indent" }
+opt.path:append({ "**" })
+opt.wildignore:append({ "*/node_modules/*" })
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "cursor"
+opt.mouse = ""
+
+opt.formatoptions:append({ "r" })
