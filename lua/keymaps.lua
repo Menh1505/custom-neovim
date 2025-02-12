48420c06504f63d07fa1419ebeaaee51db5a72bd
Menhythien <dinhthienmenh1505@gmail.com>
2025-02-12 22:47:33 +0700

    snacks & lazy keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,84 +1,166 @@
 local opts = { noremap = true, silent = true }
 local nowaitOpts = {noremap = true, silent = true, nowait = true}
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 local builtin = require('telescope.builtin')
 
--- keymap("", "", "", opts)
 -- Do not save data to clipboard when delete
-keymap("n", "c", '"_c', opts)
-keymap("n", "d", '"_d', opts)    
+map("n", "c", '"_c', opts)
+map("n", "d", '"_d', opts)    
 
--- Move text up and down
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
-keymap("v", "p", '"_dP', opts)
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
--- Copy - Past - Select all
-keymap("n", "<C-a>", "ggVG$", nowaitOpts) 
-keymap("v", "<C-c>", "y", {noremap = true, nowait = true})
-keymap("n", "<C-v>", "p", nowaitOpts)
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
--- Undo - Redo
-keymap("n", "<C-z>", "u", nowaitOpts)
-keymap("n", "<C-y>", "<C-r>", nowaitOpts)
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
--- Save
-keymap("n", "<C-s>", ":w<CR>", nowaitOpts)
+-- Move Lines
+map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+map("v", "p", '"_dP', opts)
+
+-- buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>bd", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+map("n", "<leader>bo", function()
+  Snacks.bufdelete.other()
+end, { desc = "Delete Other Buffers" })
+map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+
+-- Clear search and stop snippet on escape
+map({ "i", "n", "s" }, "<esc>", function()
+  vim.cmd("noh")
+  return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+map(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+-- save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 
 -- Quit
-keymap("n", "<C-q>", ":q<CR>", nowaitOpts)
+map("n", "<C-q>", ":q<CR>", nowaitOpts)
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
--- Better window navigation
-keymap("n", "<A-h>", "<C-w>h", opts)
-keymap("n", "<A-j>", "<C-w>j", opts)
-keymap("n", "<A-k>", "<C-w>k", opts)
-keymap("n", "<A-l>", "<C-w>l", opts)
-keymap("i", "<A-h>", "<ESC><C-w>h", opts)
-keymap("i", "<A-j>", "<ESC><C-w>j", opts)
-keymap("i", "<A-k>", "<ESC><C-w>k", opts)
-keymap("i", "<A-l>", "<ESC><C-w>l", opts)
-keymap('t', '<A-h>', [[<Cmd>wincmd h<CR>]], opts)
-keymap('t', '<A-j>', [[<Cmd>wincmd j<CR>]], opts)
-keymap('t', '<A-k>', [[<Cmd>wincmd k<CR>]], opts)
-keymap('t', '<A-l>', [[<Cmd>wincmd l<CR>]], opts)
+--keywordprg
+map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- commenting
+map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
+map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
+
+-- lazy
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+
+map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+-- formatting
+map({ "n", "v" }, "<leader>cf", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
 -- Tab
-keymap("n", "<leader>ta", ":tabnew<CR>", opts)
-keymap("n", "<leader>tn", ":tabNext<CR>", opts)
-keymap("n", "<leader>to", ":tabonly<CR>", opts)
-
--- Buffer
-keymap("n", "<leader>ba", ":badd<CR>", opts)
-keymap("n", "<leader>bd", ":bdelete<CR>", opts)
-keymap("n", "<leader>bo", ":%bd|edit#|bd#<CR>", opts)
-keymap("n", "<tab>", ":bNext<CR>", opts)
-keymap("n", "<S-tab>", ":bprevious", opts)
+map("n", "<leader>ta", ":tabnew<CR>", opts)
+map("n", "<leader>tn", ":tabNext<CR>", opts)
+map("n", "<leader>to", ":tabonly<CR>", opts)
 
 -- Press `` fast to exit insert mode 
-keymap("i", "``", "<ESC>", opts)
-keymap("v", "``", "<ESC>", opts)
+map("i", "``", "<ESC>", opts)
+map("v", "``", "<ESC>", opts)
 
--- Stay in indent mode
-keymap("v", "<", "<gv^", opts)
-keymap("v", ">", ">gv^", opts)
+-- floating terminal
+map("n", "<leader>fT", function() Snacks.terminal() end, { desc = "Terminal (cwd)" })
+map("n", "<leader>ft", function() Snacks.terminal(nil, { cwd = vim.fn.getcwd() }) end, { desc = "Terminal (CWD)" })
+map("n", "<c-/>",      function() Snacks.terminal(nil, { cwd = vim.fn.getcwd() }) end, { desc = "Terminal (CWD)" })
+map("n", "<c-_>",      function() Snacks.terminal(nil, { cwd = vim.fn.getcwd() }) end, { desc = "which_key_ignore" })
+map('t', '<C-q>', [[<C-\><C-n>]], opts) -- Exit insert mode 
 
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize +3<CR>", opts)
-keymap("n", "<C-Down>", ":resize -3<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -3<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +3<CR>", opts)
+-- Terminal Mappings
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
--- Quick terminal
--- keymap("n", "<leader>th", ":10split | term<CR>", opts)
--- keymap("n", "<leader>tv", ":50vsplit | term<CR>", opts)
-keymap('t', '<C-q>', [[<C-\><C-n>]], opts) -- Exit insert mode 
+-- windows
+map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+Snacks.toggle.zoom():map("<leader>wm"):map("<leader>uZ")
+Snacks.toggle.zen():map("<leader>uz")
 
 -- Neovim tree
-keymap("n", "<leader>e", ":NvimTreeFocus<CR>", opts)
+map("n", "<leader>e", ":NvimTreeFocus<CR>", opts)
 
 -- Find
-keymap('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-keymap('n', '<leader>fg', builtin.live_grep, { desc = 'Find live grep' })
-keymap('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
-keymap('n', '<leader>fh', builtin.help_tags, { desc = 'Find help tags' })
-keymap('n', '<leader>fm', builtin.keymaps, {desc = 'Find keymaps'})
+map('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Find live grep' })
+map('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Find help tags' })
+map('n', '<leader>fm', builtin.keymaps, {desc = 'Find keymaps'})
