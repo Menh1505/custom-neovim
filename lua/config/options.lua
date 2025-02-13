e8e8a9e11d6a40b4a2c785dd4a36ba3a4b7a3141
Menhythien <dinhthienmenh1505@gmail.com>
2025-02-13 12:23:57 +0700

    update for editor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -32,32 +32,6 @@ opt.guifont = "0xProto Nerd Font" -- Font
 opt.backup = false -- creates a backup file
 opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 
-scriptencoding = "utf-8"
-opt.encoding = "utf-8"
-opt.fileencoding = "utf-8"
-
-opt.title = true
-opt.autoindent = true
-opt.smartindent = true
-opt.hlsearch = true
-opt.showcmd = true
-opt.cmdheight = 0
-opt.laststatus = 0
-opt.expandtab = true
-opt.scrolloff = 10
-opt.inccommand = "split"
-opt.ignorecase = true
-opt.smarttab = true
-opt.breakindent = true
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.wrap = false
-opt.backspace = { "start", "eol", "indent" }
-opt.path:append({ "**" })
-opt.wildignore:append({ "*/node_modules/*" })
-opt.splitbelow = true
-opt.splitright = true
-opt.splitkeep = "cursor"
-opt.mouse = ""
+opt.mouse = "" -- Allows mouse in nvim
 
 opt.formatoptions:append({ "r" })
