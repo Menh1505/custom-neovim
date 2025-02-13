f0c218ef655cd37906bb3e3551d5a0a6443ffa01
Menhythien <dinhthienmenh1505@gmail.com>
2025-02-13 19:27:08 +0700

    googlesearch more convenient

--- lua/config/helper.lua
+++ lua/config/helper.lua
@@ -13,3 +13,38 @@ map("n", "<leader>xc", function()
     print("No diagnostic at cursor")
   end
 end, { noremap = true, silent = true, desc = "Copy diagnostic" })
+
+-- Search Google in Vim
+_G.google_search = function()
+  -- Flag watch input status
+  local input_active = true
+
+  vim.ui.input({ prompt = "Search Google: " }, function(input)
+    input_active = false -- Set false when finish input
+    if input == nil or input == "" then -- Esc or nil to quit google_search
+      vim.api.nvim_input("<Esc>")
+      return
+    end
+
+    if input and input ~= "" then
+      local encoded_query = input:gsub(" ", "+") -- Replace spacebar to "+"
+      local url = "https://www.google.com/search?q=" .. encoded_query
+      local open_cmd = "xdg-open" -- Use "open" for macOS, "start" for Windows
+      os.execute(open_cmd .. " '" .. url .. "'")
+    end
+  end)
+  -- Close if not focus
+  vim.defer_fn(function()
+    if input_active then
+      vim.api.nvim_input("<Esc>") -- ESC
+    end
+  end, 500) -- CLose after 1000ms
+end
+
+-- Keymap Google Search
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>sz",
+  ":lua _G.google_search()<CR>",
+  { noremap = true, silent = true, desc = "Search on Google" }
+)
