916b76cb73ed8d0882cf9d25da03007f86ee74da
menh <dinhthienmenh1505@gmail.com>
2025-02-16 18:22:42 +0700

    search gg for wsl

--- lua/config/helper.lua
+++ lua/config/helper.lua
@@ -17,17 +17,31 @@ end, { noremap = true, silent = true, desc = "Copy diagnostic" })
 -- Search Google in Vim
 _G.google_search = function()
   vim.ui.input({ prompt = "Search Google: " }, function(input)
-    if input == nil or input == "" then -- Esc or nil to quit google_search
+    if input == nil or input == "" then
       vim.api.nvim_input("<Esc>")
       return
     end
 
-    if input and input ~= "" then
-      local encoded_query = input:gsub(" ", "+") -- Replace spacebar to "+"
-      local url = "https://www.google.com/search?q=" .. encoded_query
-      local open_cmd = "xdg-open" -- Use "open" for macOS, "start" for Windows
-      os.execute(open_cmd .. " '" .. url .. "'")
+    local encoded_query = input:gsub(" ", "+")
+    local url = "https://www.google.com/search?q=" .. encoded_query
+
+    -- Check running on WSL
+    local open_cmd
+    local is_wsl = os.execute("grep -qi microsoft /proc/version") == 0
+
+    if is_wsl then
+      -- Check have `wslview`
+      if os.execute("command -v wslview > /dev/null") == 0 then
+        open_cmd = "wslview"
+      else
+        open_cmd = 'cmd.exe /c start "" ' -- Open default browser of Windows
+      end
+    else
+      open_cmd = "xdg-open" -- Linux
     end
+
+    -- Run command open URL
+    os.execute(open_cmd .. ' "' .. url .. '" &')
   end)
 end
 
