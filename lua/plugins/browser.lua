9008656fda5d6ac03feac3bcec02e7633dfad64f
Menhythien <dinhthienmenh1505@gmail.com>
2025-02-16 21:57:16 +0700

    search on github

--- lua/plugins/browser.lua
+++ lua/plugins/browser.lua
@@ -8,6 +8,7 @@ vim.api.nvim_create_user_command("OpenURL", function(opts)
 
   local open_cmd
 
+  -- Check OS system to open browser
   if vim.fn.has("mac") == 1 then
     open_cmd = "open"
   elseif vim.fn.has("unix") == 1 then
@@ -21,3 +22,29 @@ vim.api.nvim_create_user_command("OpenURL", function(opts)
 
   os.execute(open_cmd .. " " .. url .. " &")
 end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("Github", function(opts)
+  local query = opts.args
+
+  -- Check and auto add https:// if have not
+  if not query:match("^https?://") then
+    query = "https://github.com/search?q=" .. query
+  end
+
+  local open_cmd
+
+  -- Check OS system to open browser
+  if vim.fn.has("mac") == 1 then
+    open_cmd = "open"
+  elseif vim.fn.has("unix") == 1 then
+    open_cmd = "xdg-open"
+  elseif vim.fn.has("win32") == 1 then
+    open_cmd = "cmd.exe /c start"
+  else
+    print("Can not open URL on this system!")
+    return
+  end
+
+  -- Open URL finder on GitHub
+  os.execute(open_cmd .. " " .. query .. " &")
+end, { nargs = "*" }) -- `nargs = "*"` Allow multiple args
