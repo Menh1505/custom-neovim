82e6a87f1db2cdbc2d30fc78b3d66a04190e830f
menh <dinhthienmenh1505@gmail.com>
2025-02-16 21:39:12 +0700

    test openurl

---
+++ lua/plugins/browser.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_user_command("OpenURL", function(opts)
+  local url = opts.args
+  local open_cmd
+
+  if vim.fn.has("mac") == 1 then
+    open_cmd = "open"
+  elseif vim.fn.has("unix") == 1 then
+    open_cmd = "xdg-open"
+  elseif vim.fn.has("win32") == 1 then
+    open_cmd = "cmd.exe /c start"
+  else
+    print("Can not open URL on this system!")
+    return
+  end
+
+  os.execute(open_cmd .. " " .. url .. " &")
+end, { nargs = 1 })
